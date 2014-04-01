907c73c03e4431d56ef28b87f57edf4a3fa6082f
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-04-01 11:00:20 +0900

    support type of this or argument is date

--- date-time.js
+++ date-time.js
@@ -28,7 +28,7 @@ function pad3(n) { return n < 10 ? '00' + n : (n < 100 ? '0' + n : n); }
 // class Date : method toDateTimeString extension
 // Date+Time 日付+時刻
 function toDateTimeString(x) {
-  if (typeof x !== 'object' || !(x instanceof Date)) x = new Date();
+  x = x instanceof Date ? x : this instanceof Date ? this : new Date;
   return x.getFullYear() + '-' +
     pad2(x.getMonth() + 1) + '-' +
     pad2(x.getDate()) + ' ' +
@@ -43,7 +43,7 @@ function toDateTimeString(x) {
 
 // Date 日付
 function toDateString(x) {
-  if (typeof x !== 'object' || !(x instanceof Date)) x = new Date();
+  x = x instanceof Date ? x : this instanceof Date ? this : new Date;
   return x.getFullYear() + '-' +
     pad2(x.getMonth() + 1) + '-' +
     pad2(x.getDate());
@@ -54,7 +54,7 @@ function toDateString(x) {
 
 // Time 時刻
 function toTimeString(x) {
-  if (typeof x !== 'object' || !(x instanceof Date)) x = new Date();
+  x = x instanceof Date ? x : this instanceof Date ? this : new Date;
   return pad2(x.getHours()) + ':' +
     pad2(x.getMinutes()) + ':' +
     pad2(x.getSeconds()) + '.' +
@@ -64,35 +64,11 @@ function toTimeString(x) {
 
 //######################################################################
 
-// class Date: extend DateTime 日付時刻拡張
-function Date_toDateTimeString() {
-  return toDateTimeString(this);
-}
-
-
-//######################################################################
-
-// class Date: extend Date 日付拡張
-function Date_toDateString() {
-  return toDateString(this);
-}
-
-
-//######################################################################
-
-// class Date: extend Time 時刻拡張
-function Date_toTimeString() {
-  return toTimeString(this);
-}
-
-
-//######################################################################
-
 // class Date: extend method to*String()
 function extendDateToDateTimeString() {
-  Date.prototype.toDateTimeString = Date_toDateTimeString;
-  Date.prototype.toDateString     = Date_toDateString;
-  Date.prototype.toTimeString     = Date_toTimeString;
+  Date.prototype.toDateTimeString = toDateTimeString;
+  Date.prototype.toDateString     = toDateString;
+  Date.prototype.toTimeString     = toTimeString;
 }
 
 
@@ -100,7 +76,7 @@ function extendDateToDateTimeString() {
 
 // class Date: extend method toString()
 function extendDateToString() {
-  Date.prototype.toString         = Date_toDateTimeString;
+  Date.prototype.toString         = toDateTimeString;
 }
 
 
